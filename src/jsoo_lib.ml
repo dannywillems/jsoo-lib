5a6f6aa1ca910b91ff91529bd7770c22439a6ab5
Danny Willems <be.danny.willems@gmail.com>
2020-09-02 18:17:20 +0200

    Add BigInt and format

--- src/jsoo_lib.ml
+++ src/jsoo_lib.ml
@@ -4,161 +4,170 @@ let doc = Dom_html.document
 
 (* -------------------------------------------------------------------------- *)
 let get_input_by_id str =
-  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.input
-  (fun _ -> assert false)
+  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.input (fun _ ->
+      assert false)
 
 let get_p_by_id str =
-  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.p
-  (fun _ -> assert false)
+  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.p (fun _ ->
+      assert false)
 
 let get_div_by_id str =
-  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.div
-  (fun _ -> assert false)
+  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.div (fun _ ->
+      assert false)
 
 let get_blockquote_by_id str =
-  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.blockquote
-  (fun _ -> assert false)
+  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.blockquote (fun _ ->
+      assert false)
 
 let get_button_by_id str =
-  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.button
-  (fun _ -> assert false)
+  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.button (fun _ ->
+      assert false)
 
 let get_link_by_id str =
-  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.link
-  (fun _ -> assert false)
+  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.link (fun _ ->
+      assert false)
 
 let get_body_by_id str =
-  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.body
-  (fun _ -> assert false)
+  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.body (fun _ ->
+      assert false)
 
 let get_img_by_id str =
-  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.img
-  (fun _ -> assert false)
+  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.img (fun _ ->
+      assert false)
 
 let get_textarea_by_id str =
-  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.textarea
-  (fun _ -> assert false)
+  Js.coerce (Dom_html.getElementById str) Dom_html.CoerceTo.textarea (fun _ ->
+      assert false)
+
 (* -------------------------------------------------------------------------- *)
 
 (* -------------------------------------------------------------------------- *)
-let console_log str =
-  Firebug.console##log (Js.string str)
+let console_log str = Firebug.console##log (Js.string str)
 
-let alert str =
-  Dom_html.window##alert (Js.string str)
-(* -------------------------------------------------------------------------- *)
+let alert str = Dom_html.window##alert (Js.string str)
 
 (* -------------------------------------------------------------------------- *)
-let onload (f : (Dom_html.event Js.t -> bool Js.t)) =
+
+(* -------------------------------------------------------------------------- *)
+let onload (f : Dom_html.event Js.t -> bool Js.t) =
   Dom_html.window##.onload := Dom.handler f
-(* -------------------------------------------------------------------------- *)
 
-(* -------------------------------------------------------------------------- *)
-module Head =
-  struct
-    (* FIXME: external lib for listing media_type *)
-    type media_type =
-      | Text_css
-      | Text_js
-      | No_media_type
-
-    type rel =
-      | Alternate
-      | Archives
-      | Author
-      | Bookmark
-      | External
-      | First
-      | Help
-      | Icon
-      | Last
-      | License
-      | Next
-      | Nofollow
-      | Noreferrer
-      | Pingback
-      | Prefetch
-      | Prev
-      | Search
-      | Sidebar
-      | Stylesheet
-      | Tag
-      | Up
-
-    type cross_origin =
-      | Anonymous
-      | Use_credentials
-      | Default_cross_origin (* Empty argument *)
-
-    let media_type_to_str s = match s with
-      | Text_css -> "text/css"
-      | Text_js -> "text/js"
-      | No_media_type -> ""
-
-    let rel_to_str s = match s with
-      | Alternate -> "alternate"
-      | Archives -> "archives"
-      | Author -> "author"
-      | Bookmark -> "bookmark"
-      | External -> "external"
-      | First -> "first"
-      | Help -> "help"
-      | Icon -> "icon"
-      | Last -> "last"
-      | License -> "license"
-      | Next -> "next"
-      | Nofollow -> "nofollow"
-      | Noreferrer -> "noreferrer"
-      | Pingback -> "pingback"
-      | Prefetch -> "prefetch"
-      | Prev -> "prev"
-      | Search -> "search"
-      | Sidebar -> "sidebar"
-      | Stylesheet -> "stylesheet"
-      | Tag -> "tag"
-      | Up -> "up"
-
-    let cross_origin_to_str s = match s with
-      | Anonymous -> "anonymous"
-      | Use_credentials -> "use-credentials"
-      | Default_cross_origin -> ""
-
-    let add_link
-      ?(media_type=No_media_type) ?(cross_origin=Default_cross_origin)
-      ?(language_code="") ?(media_query="") ~rel href =
-      let l = Dom_html.createLink doc in
-      l##.crossorigin := Js.string (cross_origin_to_str cross_origin);
-      l##.href := Js.string href;
-      if language_code <> "" then l##.hreflang := Js.string (language_code);
-      if media_query <> "" then l##.media := Js.string (media_query);
-      l##.rel := Js.string (rel_to_str rel);
-      l##._type := Js.string (media_type_to_str media_type);
-      Dom.appendChild doc##.head l
-
-    let add_css_link href =
-      add_link ~media_type:Text_css ~rel:Stylesheet href
-
-    let add_js_script l =
-      let script = Dom_html.createScript doc in
-      script##.src := Js.string l;
-      script##._type := Js.string "text/javascript";
-      Dom.appendChild doc##.head script
-  end
 (* -------------------------------------------------------------------------- *)
 
 (* -------------------------------------------------------------------------- *)
-module Body =
-  struct
-    let add_js_script l =
-      let script = Dom_html.createScript doc in
-      script##.src := Js.string l;
-      script##._type := Js.string "text/javascript";
-      Dom.appendChild doc##.body script
+module Head = struct
+  (* FIXME: external lib for listing media_type *)
+  type media_type = Text_css | Text_js | No_media_type
 
-    let rec append_child_mult a l =
-      match l with
-      | [] -> ()
-      | head::tail -> Dom.appendChild a head; append_child_mult a tail
+  type rel =
+    | Alternate
+    | Archives
+    | Author
+    | Bookmark
+    | External
+    | First
+    | Help
+    | Icon
+    | Last
+    | License
+    | Next
+    | Nofollow
+    | Noreferrer
+    | Pingback
+    | Prefetch
+    | Prev
+    | Search
+    | Sidebar
+    | Stylesheet
+    | Tag
+    | Up
 
-  end
+  type cross_origin = Anonymous | Use_credentials | Default_cross_origin
+
+  (* Empty argument *)
+
+  let media_type_to_str s =
+    match s with
+    | Text_css -> "text/css"
+    | Text_js -> "text/js"
+    | No_media_type -> ""
+
+  let rel_to_str s =
+    match s with
+    | Alternate -> "alternate"
+    | Archives -> "archives"
+    | Author -> "author"
+    | Bookmark -> "bookmark"
+    | External -> "external"
+    | First -> "first"
+    | Help -> "help"
+    | Icon -> "icon"
+    | Last -> "last"
+    | License -> "license"
+    | Next -> "next"
+    | Nofollow -> "nofollow"
+    | Noreferrer -> "noreferrer"
+    | Pingback -> "pingback"
+    | Prefetch -> "prefetch"
+    | Prev -> "prev"
+    | Search -> "search"
+    | Sidebar -> "sidebar"
+    | Stylesheet -> "stylesheet"
+    | Tag -> "tag"
+    | Up -> "up"
+
+  let cross_origin_to_str s =
+    match s with
+    | Anonymous -> "anonymous"
+    | Use_credentials -> "use-credentials"
+    | Default_cross_origin -> ""
+
+  let add_link ?(media_type = No_media_type)
+      ?(cross_origin = Default_cross_origin) ?(language_code = "")
+      ?(media_query = "") ~rel href =
+    let l = Dom_html.createLink doc in
+    l##.crossorigin := Js.string (cross_origin_to_str cross_origin) ;
+    l##.href := Js.string href ;
+    if language_code <> "" then l##.hreflang := Js.string language_code ;
+    if media_query <> "" then l##.media := Js.string media_query ;
+    l##.rel := Js.string (rel_to_str rel) ;
+    l##._type := Js.string (media_type_to_str media_type) ;
+    Dom.appendChild doc##.head l
+
+  let add_css_link href = add_link ~media_type:Text_css ~rel:Stylesheet href
+
+  let add_js_script l =
+    let script = Dom_html.createScript doc in
+    script##.src := Js.string l ;
+    script##._type := Js.string "text/javascript" ;
+    Dom.appendChild doc##.head script
+end
+
 (* -------------------------------------------------------------------------- *)
+
+(* -------------------------------------------------------------------------- *)
+module Body = struct
+  let add_js_script l =
+    let script = Dom_html.createScript doc in
+    script##.src := Js.string l ;
+    script##._type := Js.string "text/javascript" ;
+    Dom.appendChild doc##.body script
+
+  let rec append_child_mult a l =
+    match l with
+    | [] -> ()
+    | head :: tail ->
+        Dom.appendChild a head ;
+        append_child_mult a tail
+end
+
+(* -------------------------------------------------------------------------- *)
+
+module BigInt = struct
+  type t = Js.Unsafe.any Js.t
+
+  let of_int x =
+    Js.Unsafe.fun_call
+      (Js.Unsafe.js_expr "BigInt")
+      [| Js.Unsafe.inject (Js.string (string_of_int x)) |]
+end
--- src/jsoo_lib.mli
+++ src/jsoo_lib.mli
@@ -1,74 +1,89 @@
 open Js_of_ocaml
 
 val get_input_by_id : string -> Dom_html.inputElement Js.t
+
 val get_p_by_id : string -> Dom_html.paramElement Js.t
+
 val get_div_by_id : string -> Dom_html.divElement Js.t
+
 val get_blockquote_by_id : string -> Dom_html.quoteElement Js.t
+
 val get_button_by_id : string -> Dom_html.buttonElement Js.t
+
 val get_link_by_id : string -> Dom_html.linkElement Js.t
+
 val get_body_by_id : string -> Dom_html.bodyElement Js.t
+
 val get_img_by_id : string -> Dom_html.imageElement Js.t
+
 val get_textarea_by_id : string -> Dom_html.textAreaElement Js.t
 
 val console_log : string -> unit
+
 val alert : string -> unit
 
 val onload : (Dom_html.event Js.t -> bool Js.t) -> unit
 
 (* See http://www.w3schools.com/tags/tag_link.asp *)
-module Head :
-  sig
-    (* FIXME: external lib for listing media_type *)
-    type media_type =
-      | Text_css
-      | Text_js
-      | No_media_type
+module Head : sig
+  (* FIXME: external lib for listing media_type *)
+  type media_type = Text_css | Text_js | No_media_type
 
-    type rel =
-      | Alternate
-      | Archives
-      | Author
-      | Bookmark
-      | External
-      | First
-      | Help
-      | Icon
-      | Last
-      | License
-      | Next
-      | Nofollow
-      | Noreferrer
-      | Pingback
-      | Prefetch
-      | Prev
-      | Search
-      | Sidebar
-      | Stylesheet
-      | Tag
-      | Up
+  type rel =
+    | Alternate
+    | Archives
+    | Author
+    | Bookmark
+    | External
+    | First
+    | Help
+    | Icon
+    | Last
+    | License
+    | Next
+    | Nofollow
+    | Noreferrer
+    | Pingback
+    | Prefetch
+    | Prev
+    | Search
+    | Sidebar
+    | Stylesheet
+    | Tag
+    | Up
 
-    type cross_origin =
-      | Anonymous
-      | Use_credentials
-      | Default_cross_origin (* Empty argument *)
+  type cross_origin = Anonymous | Use_credentials | Default_cross_origin
 
-    (* Charset, rev and target not implemented because deprecated in HTML5 *)
-    val add_link :  ?media_type:media_type       ->
-                    ?cross_origin:cross_origin  ->
-                    ?language_code:string       -> (* FIXME: Sum type! *)
-                    ?media_query:string         -> (* FIXME: Sum type! *)
-                    rel:rel                     ->
-                    string                      ->
-                    unit
+  (* Empty argument *)
 
-    val add_css_link : string -> unit
+  (* Charset, rev and target not implemented because deprecated in HTML5 *)
+  val add_link :
+    ?media_type:media_type ->
+    ?cross_origin:cross_origin ->
+    ?language_code:string ->
+    (* FIXME: Sum type! *)
+    ?media_query:string ->
+    (* FIXME: Sum type! *)
+    rel:rel ->
+    string ->
+    unit
 
-    val add_js_script : string -> unit
-  end
+  val add_css_link : string -> unit
 
-module Body :
-  sig
-    val add_js_script : string -> unit
+  val add_js_script : string -> unit
+end
 
-    val append_child_mult : #Dom.node Js.t -> #Dom.node Js.t list -> unit
-  end
+module Body : sig
+  val add_js_script : string -> unit
+
+  val append_child_mult : #Dom.node Js.t -> #Dom.node Js.t list -> unit
+end
+
+(** Binding to the BigInt class *)
+module BigInt : sig
+  (** A BigInt object *)
+  type t
+
+  (** Create a BigInt value from an integer *)
+  val of_int : int -> t
+end
