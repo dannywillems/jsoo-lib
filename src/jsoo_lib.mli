3ab77d540a31b9f3e4df3eb90310f7b8155bc610
Danny Willems <be.danny.willems@gmail.com>
2020-09-10 15:51:45 +0200

    Improve documentation

--- src/jsoo_lib.mli
+++ src/jsoo_lib.mli
@@ -7,29 +7,41 @@ module type JS_OBJECT = sig
 
   val to_any_js : t -> Js.Unsafe.any
 
+  (** Equivalent to [toString] on the object in JavaScript *)
   val to_string : t -> string
 end
 
+(** Typed version of [getElementById], coercing to an input element *)
 val get_input_by_id : string -> Dom_html.inputElement Js.t
 
+(** Typed version of [getElementById], coercing to a paragraph element *)
 val get_p_by_id : string -> Dom_html.paramElement Js.t
 
+(** Typed version of [getElementById], coercing to a div element *)
 val get_div_by_id : string -> Dom_html.divElement Js.t
 
+(** Typed version of [getElementById], coercing to a blockquote element *)
 val get_blockquote_by_id : string -> Dom_html.quoteElement Js.t
 
+(** Typed version of [getElementById], coercing to a button element *)
 val get_button_by_id : string -> Dom_html.buttonElement Js.t
 
+(** Typed version of [getElementById], coercing to a link element *)
 val get_link_by_id : string -> Dom_html.linkElement Js.t
 
+(** Typed version of [getElementById], coercing to the body element *)
 val get_body_by_id : string -> Dom_html.bodyElement Js.t
 
+(** Typed version of [getElementById], coercing to an image element *)
 val get_img_by_id : string -> Dom_html.imageElement Js.t
 
+(** Typed version of [getElementById], coercing to a textarea element *)
 val get_textarea_by_id : string -> Dom_html.textAreaElement Js.t
 
+(** [console_log s] logs [s] to the console *)
 val console_log : string -> unit
 
+(** [alert s] creates an alert with the string [s] *)
 val alert : string -> unit
 
 val onload : (Dom_html.event Js.t -> bool Js.t) -> unit
@@ -64,8 +76,6 @@ module Head : sig
 
   type cross_origin = Anonymous | Use_credentials | Default_cross_origin
 
-  (* Empty argument *)
-
   (* Charset, rev and target not implemented because deprecated in HTML5 *)
   val add_link :
     ?media_type:media_type ->
@@ -78,12 +88,15 @@ module Head : sig
     string ->
     unit
 
+  (** Add a css asset in the header *)
   val add_css_link : string -> unit
 
+  (** Add a JavaScript script in the header *)
   val add_js_script : string -> unit
 end
 
 module Body : sig
+  (** Add a JavaScript script in the body *)
   val add_js_script : string -> unit
 
   val append_child_mult : #Dom.node Js.t -> #Dom.node Js.t list -> unit
@@ -93,20 +106,20 @@ end
 module BigInt : sig
   include JS_OBJECT
 
-  (** Create a BigInt value from an integer *)
+  (** Create a [BigInt] value from an integer *)
   val of_int : int -> t
 
   val zero : t
 
   val one : t
 
-  (** Create a BigInt value from a string *)
+  (** Create a [BigInt] value from a string *)
   val of_string : string -> t
 
-  (** Create a BigInt value from a UInt64 value *)
+  (** Create a [BigInt] value from a UInt64 value *)
   val of_uint64 : Unsigned.UInt64.t -> t
 
-  (** Create a BigInt value from a UInt32 value *)
+  (** Create a [BigInt] value from a UInt32 value *)
   val of_uint32 : Unsigned.UInt32.t -> t
 
   (** [is_bigint js_obj] returns [true] if [js_obj] is of type [BigInt], else
@@ -127,8 +140,8 @@ end
 module Number : sig
   include JS_OBJECT
 
-  (** Create a Number value from an integer. The value is not verified to be in
-  the interval allowed for a Number object *)
+  (** Create a [Number] value from an integer. The value is not verified to be in
+  the interval allowed for a [Number] object *)
   val of_int : int -> t
 
   val zero : t
